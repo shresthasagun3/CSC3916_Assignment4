0b1d15c9e55dfbe4a1e501c8b6432e6164716668
shresthasagun3 <ssagun336@gmail.com>
2026-04-05 14:06:12 -0600

    .env file

--- Users.js
+++ Users.js
@@ -8,7 +8,7 @@ mongoose.Promise = global.Promise;
 try {
     mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
         console.log("connected"));
-}catch (error) {
+} catch (error) {
     console.log("could not connect");
 }
 mongoose.set('useCreateIndex', true);
